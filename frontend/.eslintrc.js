ed4252e86b2d830a2f9c8db0f4007948bb3415c5
Omar Baradei <omarbaradei21@gmail.com>
2020-01-07 01:51:46 -0800

    Added dist_electron to gitignore, modified eslintrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,11 +13,10 @@ module.exports = {
     },
     extends: [
         // use the recommended rule set for both plain javascript and vue
-        // 'eslint:recommended',
         'plugin:vue/recommended',
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     ],
     rules: {
         // we should always disable console logs and debugging in production
